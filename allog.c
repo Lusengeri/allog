23c2c482679b2da939e1ac1fc688e2a02349b0e1
Lusengeri <lusengeri@gmail.com>
2021-03-02 07:10:12 +0300

    Added syslog handler

--- allog.c
+++ allog.c
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <syslog.h>
 
 char *level_strings[] = {"NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"};
 char *get_current_time_string();
@@ -27,16 +28,18 @@ void add_handler(struct log_obj *logger, void *handler)
 }
 
 void log_message(struct log_obj *logger, log_lev level, char *log_string, ...)
-{	
+{
+	if (level < logger->level)
+		return;
+
 	va_list arg_ptr;
 	va_start(arg_ptr, log_string);
 
 	obj_list_node *iter;	
 	for (iter = (logger->handler_list->head.next_obj); (iter->this_obj) != NULL; iter = iter->next_obj) {
-		struct log_handler * handler = (struct log_handler*)(iter->this_obj);
+		struct log_handler *handler = (struct log_handler*) (iter->this_obj);
 		handler->log_to(handler, level, log_string, arg_ptr);
 	}
-	//logger->handler->log_to(logger->handler, level, log_string, arg_ptr);
 
 	va_end(arg_ptr);
 	return;
@@ -51,6 +54,34 @@ void log_message_to_file(struct log_handler *handler, log_lev level, char *log_s
 	return;
 }
 
+
+void log_message_to_syslog(struct log_handler *handler, log_lev level, char *log_string, va_list arg_ptr)
+{
+	int priority;
+
+	switch (level){
+		case DEBUG: 
+			priority = LOG_DEBUG;
+			break;
+		case INFO: 
+			priority = LOG_INFO;
+			break;
+		case WARNING:
+			priority = LOG_WARNING;
+			break;
+		case ERROR:
+			priority = LOG_ERR;
+			break;
+		case CRITICAL:
+			priority = LOG_CRIT;
+			break;
+		default:
+			priority = LOG_DEBUG;
+	}
+
+	vsyslog(priority, log_string, arg_ptr);
+
+}
 struct file_handler* new_file_handler(char *file_name)
 {
 	struct file_handler *new = malloc(sizeof(struct file_handler));
@@ -65,6 +96,21 @@ struct file_handler* new_file_handler(char *file_name)
 	new->base.log_to = log_message_to_file;
 	new->file = log_file;
 	return new;
+}
+
+struct file_handler* new_stream_handler(FILE *file_stream)
+{
+	struct file_handler *new = malloc(sizeof(struct file_handler));
+	//What error handling's required here?
+	new->base.log_to = log_message_to_file;
+	new->file = file_stream;
+	return new;
+}
+
+struct log_handler* new_syslog_handler()
+{
+	struct log_handler *new = malloc(sizeof(struct log_handler));
+	new->log_to = log_message_to_syslog;
 }
 
 char *get_current_time_string()
--- allog.h
+++ allog.h
@@ -21,7 +21,7 @@ typedef struct log_obj {
 	struct log_handler *handler;
 	obj_list *handler_list;
 	void (*add_handler)(struct log_obj*, struct log_handler*);
-	void (*set_level) (struct log_obj*, log_lev);
+	void (*set_level)(struct log_obj*, log_lev);
 	log_lev (*get_effective_level)(struct log_obj*);
 	bool (*is_enabled_for_level)(struct log_obj*, log_lev);
 } log_obj;
@@ -30,7 +30,8 @@ log_obj *new_log_obj(log_lev);
 void set_level(struct log_obj*, log_lev);
 void add_handler(struct log_obj*, void* handler);
 struct file_handler* new_file_handler(char *file_name);
-//struct file_handler* new_stream_handler(FILE*);
-//struct file_handler* new_syslog_handler();
+struct file_handler* new_stream_handler(FILE*);
+struct log_handler* new_syslog_handler();
 void log_message(struct log_obj *log, log_lev level, char *log_string, ...);
+
 #endif
